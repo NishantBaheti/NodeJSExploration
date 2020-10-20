aee41fb1dbf8d54074297c5c20b29017f561e6be
Nishant Baheti <nishantbaheti.it19@gmail.com>
2020-10-20 23:45:59 +0530

    Progress commit

--- ExpressDemo/index.js
+++ ExpressDemo/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const Joi = require("joi");
 
 const courses = [
   {
@@ -12,6 +13,8 @@ const courses = [
     name: "DL",
   },
 ];
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!!");
@@ -37,7 +40,25 @@ app.get("/api/posts/:year/:month", (req, res) => {
   res.send(req.params);
 });
 
+app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const { result, error } = schema.validate(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(course);
+  res.send(courses);
+});
+
 app.listen(port, () => {
-  it;
   console.log(`listening to port ${port} ...`);
 });
